1d69bf6a0ecaded1bc1860a660a5e5515064097c
David Orn <orn.david@gmail.com>
2022-08-27 10:36:22 +1000

    Adjusted width/height of the bars to be (possibly) consistent

--- prettyplotter/pretty_plots.py
+++ prettyplotter/pretty_plots.py
@@ -44,7 +44,7 @@ def remove_spines(ax: mpl.axes.Axes) -> None:
         ax.spines[location].set_visible(False)
 
 
-def gray_bars(data :dict, order = False, **kwargs) -> Tuple[mpl.figure.Figure, mpl.axes.Axes]:
+def gray_bars(data :dict, **kwargs) -> Tuple[mpl.figure.Figure, mpl.axes.Axes]:
     """ 
     Gray bars create a sorted side-view bar graph with the keys (names)
     on the left side of the graph and the values shown. Pick the order 
@@ -53,16 +53,29 @@ def gray_bars(data :dict, order = False, **kwargs) -> Tuple[mpl.figure.Figure, m
     Returns the figure handle and axes 
 
     **kwargs 
+        'font' -> Dictionary to configure fonts using matplotlib.rc
+        'order'-> True for ascending false for descending (default True)
+        
     E.g. set specific font type
-    """
-    if "font" in kwargs:
+    """ 
+    if 'font' in kwargs:
         set_font(kwargs['font'])
     else:
         default_font()
+    if 'order' in kwargs:
+        order = kwargs['order']
+    else:
+        order = True
+    if 'max_height' in kwargs:
+        max_height = kwargs['max_height']
+    else:
+        max_height = None
+
 
     if not isinstance(order, bool):
         raise ValueError(f'{__name__} - "order" must be boolean')
-
+    
+    no_values = len(data)
     names = []
     height_values = []
     for key in sorted(data, key=data.get, reverse=order):
@@ -70,13 +83,28 @@ def gray_bars(data :dict, order = False, **kwargs) -> Tuple[mpl.figure.Figure, m
         height_values.append(data[key])
 
     height_values = np.array(height_values)
-    colormap = gray_sequentials(len(height_values))
-    x_base_location = np.arange(len(height_values))
+    colormap = gray_sequentials(no_values)
+
+    # Compute a pretty looking ratio between space and width of 
+    # column
+    width = 0.25
+    padding = 0.05
+    y_base_location = np.zeros((no_values, ))
+    for i in range(1, no_values):
+        y_base_location[i] = y_base_location[i-1] + (width+padding)
 
     fig, ax = normalised_figure()
-    ax.barh(x_base_location, height_values, color=colormap, tick_label=names)
+    ax.barh(y_base_location, 
+            height_values, 
+            width,
+            color=colormap, 
+            tick_label=names)
+
     remove_spines(ax)
     xaxis_grid(ax)
+
+    if max_height:
+        ax.set_ylim([-1, max_height*(width+padding)+1])
 
     return fig, ax
 
